47768ef60cc9ca2d27dedefcc4ca285fe934ba8e
Mohamed iheb hichri <58292737+mediheb1920@users.noreply.github.com>
2020-10-29 21:48:48 +0100

    Updates

---
+++ precache-manifest.9c998f4eeabbc0df85f1d75a916cd116.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccc83eef6fe34428ddd2fd3f3031203",
+    "url": "/medIheb20.github.io/index.html"
+  },
+  {
+    "revision": "a556174d3a79d3f492ec",
+    "url": "/medIheb20.github.io/static/css/main.0575425f.chunk.css"
+  },
+  {
+    "revision": "01331c1a605d2d94fcb7",
+    "url": "/medIheb20.github.io/static/js/2.34fa651c.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/medIheb20.github.io/static/js/2.34fa651c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a556174d3a79d3f492ec",
+    "url": "/medIheb20.github.io/static/js/main.605cb39e.chunk.js"
+  },
+  {
+    "revision": "edc4515059236615acc0",
+    "url": "/medIheb20.github.io/static/js/runtime-main.7a9cbc94.js"
+  },
+  {
+    "revision": "ad29a142be895822d750ab6321a72654",
+    "url": "/medIheb20.github.io/static/media/crwen-shop.ad29a142.png"
+  },
+  {
+    "revision": "1604474b84b7ae8a2bd16db3664c0af4",
+    "url": "/medIheb20.github.io/static/media/cv.1604474b.svg"
+  },
+  {
+    "revision": "6a4d1055b393e3709b5c1db79c625f31",
+    "url": "/medIheb20.github.io/static/media/eatEasy.6a4d1055.png"
+  },
+  {
+    "revision": "20ea221cfeaa82d9a8cefe84dadec258",
+    "url": "/medIheb20.github.io/static/media/kprm.20ea221c.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/medIheb20.github.io/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "cd147b113662bda2281f6cb36b64b62b",
+    "url": "/medIheb20.github.io/static/media/mcv.cd147b11.pdf"
+  },
+  {
+    "revision": "922fdc034ca28ad5ef4f1d541b6261e3",
+    "url": "/medIheb20.github.io/static/media/menu.922fdc03.svg"
+  },
+  {
+    "revision": "4d3195848f5038a69a5eedfd3ddefdcd",
+    "url": "/medIheb20.github.io/static/media/project.4d319584.svg"
+  },
+  {
+    "revision": "da5d98f1a6d6909bf08f313020de2716",
+    "url": "/medIheb20.github.io/static/media/skill.da5d98f1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa96ce42d4ec1780bb04ed9c96c74b06.js"
+  "/medIheb20.github.io/precache-manifest.9c998f4eeabbc0df85f1d75a916cd116.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/medIheb20.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
