e7a550dfb7da4fbab9395043d40d22d613216da9
Mohamed iheb hichri <58292737+mediheb1920@users.noreply.github.com>
2020-10-29 19:02:53 +0100

    Updates

---
+++ precache-manifest.ae21e6734bc190a38220e7f8761fd29f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad86556d06bb1dc77b7d0797a35548d",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "05424338c10ce394c6ef",
+    "url": "/home/static/css/main.96016ed6.chunk.css"
+  },
+  {
+    "revision": "8981c1d87b3cc7796d40",
+    "url": "/home/static/js/2.13b64b40.chunk.js"
+  },
+  {
+    "revision": "a969063ca8d1f703b12d93a738ec6ae4",
+    "url": "/home/static/js/2.13b64b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05424338c10ce394c6ef",
+    "url": "/home/static/js/main.511914f8.chunk.js"
+  },
+  {
+    "revision": "5c5433d483d7af46e04b",
+    "url": "/home/static/js/runtime-main.fedc248f.js"
+  },
+  {
+    "revision": "ad29a142be895822d750ab6321a72654",
+    "url": "/home/static/media/crwen-shop.ad29a142.png"
+  },
+  {
+    "revision": "1604474b84b7ae8a2bd16db3664c0af4",
+    "url": "/home/static/media/cv.1604474b.svg"
+  },
+  {
+    "revision": "6a4d1055b393e3709b5c1db79c625f31",
+    "url": "/home/static/media/eatEasy.6a4d1055.png"
+  },
+  {
+    "revision": "20ea221cfeaa82d9a8cefe84dadec258",
+    "url": "/home/static/media/kprm.20ea221c.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/home/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cd147b113662bda2281f6cb36b64b62b",
+    "url": "/home/static/media/mcv.cd147b11.pdf"
+  },
+  {
+    "revision": "922fdc034ca28ad5ef4f1d541b6261e3",
+    "url": "/home/static/media/menu.922fdc03.svg"
+  },
+  {
+    "revision": "4d3195848f5038a69a5eedfd3ddefdcd",
+    "url": "/home/static/media/project.4d319584.svg"
+  },
+  {
+    "revision": "da5d98f1a6d6909bf08f313020de2716",
+    "url": "/home/static/media/skill.da5d98f1.svg"
+  }
+]);
