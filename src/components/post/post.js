d92f1f71c19e11a8170b2be817a01cfd52a159b4
Mohamed iheb hichri <58292737+mediheb1920@users.noreply.github.com>
2020-10-30 14:56:43 +0100

    mh-fix-the-posts

--- src/components/post/post.js
+++ src/components/post/post.js
@@ -8,7 +8,7 @@ import './post.scss';
 export const PostInstagram = ({postLink, imagePost}) => {
 
     return(
-        <a  href={postLink} target='_blank' rel="noopener noreferrer"  >
+        <a className='link-post'  href={postLink} target='_blank' rel="noopener noreferrer"  >
             <div 
                 className='container-post' 
                 style={{ backgroundImage: `url(${imagePost})`}}
